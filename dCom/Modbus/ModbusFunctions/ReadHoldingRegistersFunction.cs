89351dbecb426bad82739b9f39a17ea6f27bc328
sofijarabrenovic <rabrenovicsofija0@gmail.com>
2025-09-01 16:56:41 +0200

    read holding reg

--- dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -24,15 +24,42 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] zahtev = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.TransactionId)), 0, zahtev, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.ProtocolId)), 0, zahtev, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.Length)), 0, zahtev, 4, 2);
+            zahtev[6] = CommandParameters.UnitId;
+            zahtev[7] = CommandParameters.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).StartAddress)), 0, zahtev, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).Quantity)), 0, zahtev, 10, 2);
+
+            return zahtev;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            var povratna = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if (response[7] == CommandParameters.FunctionCode + 0x80)
+            {
+                HandeException(response[8]);
+            }else
+            {
+                ushort adresa = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+                for(int i = 0; i < response[8]/2; i++)
+                {
+                    byte byte1 = response[8 + 1 + i * 2];
+                    byte byte2 = response[8 + 2 + i * 2];
+                    ushort vrednost1 = BitConverter.ToUInt16(new byte[2] { byte2, byte1 }, 0);
+
+                    povratna.Add(new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, adresa), vrednost1);
+                    adresa++;
+                }
+            }
+            return povratna;
         }
     }
 }
