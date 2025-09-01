20d353a0e4c8c8e66989476caaecec80dc263d25
sofijarabrenovic <rabrenovicsofija0@gmail.com>
2025-09-01 17:31:14 +0200

    write coil

--- dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -24,15 +24,41 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] zahtev = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.TransactionId)), 0, zahtev, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.ProtocolId)), 0, zahtev, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.Length)), 0, zahtev, 4, 2);
+            zahtev[6] = CommandParameters.UnitId;
+            zahtev[7] = CommandParameters.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusWriteCommandParameters)CommandParameters).OutputAddress)), 0, zahtev, 8, 2);
+            ushort vrednost = ((ModbusWriteCommandParameters)CommandParameters).Value == 0
+            ? (ushort)0x0000 //0
+            : (ushort)0xFF00; //1
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)vrednost)), 0, zahtev, 10, 2);
+            return zahtev;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            var povratna = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            if (response[7] == CommandParameters.FunctionCode + 0x80)
+            {
+                HandeException(response[8]);
+            }
+            else
+            {
+                var adresa = (ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(response, 8));
+                var vrednost = (ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(response, 10));
+
+                ushort vrednost1 = (vrednost == 0xFF00) ? (ushort)1 : (ushort)0;
+
+                Tuple<PointType, ushort> kljuc = new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, adresa);
+                povratna.Add(kljuc, vrednost1);
+
+            }
+            return povratna;
         }
     }
 }
